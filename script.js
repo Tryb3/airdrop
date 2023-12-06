61519eae9fdf423134ebd18b0fcf2b376d293987
Tryb3 <139131887+Tryb3@users.noreply.github.com>
2023-12-06 04:40:35 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function discoverNow() {
+    // Add JavaScript logic for the "Discover Now" button
+    alert("Discover Now button clicked!");
+    // You can redirect the user or perform other actions as needed
+}
+
+function claimAirdrop() {
+    // Add JavaScript logic for claiming the airdrop
+    // This could include form validation, API calls, etc.
+    alert("Airdrop claimed successfully!");
+    // You can redirect the user or perform other actions as needed
+}
